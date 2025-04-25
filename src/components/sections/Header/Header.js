0ae8526108e56bba43fec270266f271fad5ae139
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-04-26 00:55:53 +0200

    Add menu elements

--- src/components/sections/Header/Header.js
+++ src/components/sections/Header/Header.js
@@ -3,10 +3,32 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <h1>Mi Portafolio</h1>
       <nav>
-        <a href="#about">Sobre mí</a>
-        <a href="#projects">Proyectos</a>
+        <div>
+        <h1>Fredy Izquierdo</h1>
+        </div>
+        <a href='#' className='navbar-toggler'>
+          <span className='navbar-toggle-icon'>icono hambuerguesa</span>
+        </a>
+        <div className='collapse navbar-collapse' id='navbar-menus'>
+          <ul className='navbar-menus-list'>
+            <li className='nav-item'>
+              <a href="#home" className='nav-link active'>Home</a>
+            </li>
+            <li className='nav-item'>
+              <a href="#about" className='nav-link'>About</a>
+            </li>
+            <li className='nav-item'>
+              <a href="#projects" className='nav-link'>Projects</a>
+            </li>
+            <li className='nav-item'>
+              <a href="#contact" className='nav-link'>Contact</a>
+            </li>
+            <li className='nav-item'>
+              <a href="#resume" className='nav-link resume'>Resume</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
