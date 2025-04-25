59fd233434ea8545d5aaee3f786ef3555aaa6adc
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-04-25 22:17:18 +0200

    Add comment with resources to read later

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './components/sections/About/About';
 import Projects from './components/sections/Projects/Projects';
 import './App.css';
 
+// portfolio ejemplo: https://github.com/berthutapea/berthutapea-portfolio
 function App() {
   return (
     <div className="portfolio-app">
