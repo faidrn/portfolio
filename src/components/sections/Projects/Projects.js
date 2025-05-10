a87ff1918500a81a48d122bee9fd017000385c5e
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-05-10 22:41:38 +0200

    feat: Add Project compenent, showing the list projects

--- src/components/sections/Projects/Projects.js
+++ src/components/sections/Projects/Projects.js
@@ -5,7 +5,7 @@ import './Projects.css';
 const Projects = () => {
   return (
     <section id="projects" className="projects-section">
-      <h2>Mis Proyectos</h2>
+      <h2 className="section-title-projects">My Projects</h2>
       <div className="projects-grid">
         {projects.map((project) => (
           <div key={project.id} className="project-card">
@@ -14,10 +14,26 @@ const Projects = () => {
             <div className="technologies">
               {project.technologies.join(' • ')}
             </div>
-            <Button 
-              text="Ver Demo" 
-              onClick={() => window.open(project.demoUrl, '_blank')} 
-            />
+            <div className='buttons-container'>
+                <Button 
+                text="Repository" 
+                className="resume"
+                onClick={() => {
+                  window.open(project.githubUrl, '_blank', 'noopener, noreferrer')
+                }} 
+              />
+              {/* Oculto el componente Button cuando la variable demoUrl esta vacia */}
+              {project.demoUrl && 
+                <Button 
+                  text="Ver Demo" 
+                  className="resume"
+                  onClick={() => {
+                    window.open(project.demoUrl, '_blank', 'noopener, noreferrer')
+                  }} 
+                />
+              }
+              
+            </div>
           </div>
         ))}
       </div>
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,17 +2,138 @@ export const projects = [
     {
       id: 1,
       title: "Tic-Tac-Toe Game",
-      description: "Juego interactivo con React",
-      technologies: ["React", "JavaScript"],
+      description: "This project is an implementation of the classic Tic-Tac-Toe game based on the official React tutorial. The tutorial walks through fundamental React concepts while building this interactive game.",
+      technologies: ["React", "JavaScript", "CSS"],
       githubUrl: "https://github.com/faidrn/tic-tac-toe-game",
       demoUrl: "https://faidrn.github.io/tic-tac-toe-game/"
     },
     {
       id: 2,
       title: "Portafolio Personal",
-      description: "Mi sitio web profesional",
-      technologies: ["React", "CSS Modules"],
+      description: "My professional website: A modern, responsive portfolio built with React.js that showcases my projects, skills, and experience as a front-end developer. It includes a dynamic project gallery with live demos and code links, a clean user interface (UI)/user experience (UX) with elegant animations, a mobile-optimized design, and an easy-to-use contact form integrated with email.js or the API.",
+      technologies: ["React", "CSS", "JavaScript"],
       githubUrl: "https://github.com/faidrn/portfolio",
       demoUrl: "https://faidrn.github.io/portafolio/"
+    },
+    {
+      id: 3,
+      title: "ToDo List",
+      description: "A React.js learning repository for beginners that covers basic concepts like components, state, props, and hooks through practical examples. This project includes structured exercises and small apps like a to-do list to help beginners understand the fundamentals of React. Ideal for developers just starting out with React.",
+      technologies: ["React", "CSS", "JavaScript"],
+      githubUrl: "https://github.com/faidrn/curso-react-intro",
+      demoUrl: "https://faidrn.github.io/curso-react-intro/"
+    },
+    {
+      id: 4,
+      title: "React Render Patterns",
+      description: "A practical repository exploring advanced React rendering techniques and performance optimization patterns. It covers key concepts such as conditional rendering, composite components, and rendering properties through practical examples.",
+      technologies: ["React", "CSS", "JavaScript"],
+      githubUrl: "https://github.com/faidrn/curso-react-patrones-render",
+      demoUrl: "https://faidrn.github.io/curso-react-patrones-render/"
+    },
+    {
+      id: 5,
+      title: "Instagram Clone",
+      description: "This is a simplified version of the popular social media platform Instagram, for educational purposes only, applying the knowledge acquired in learning Frontend.",
+      technologies: ["React", "CSS", "JavaScript"],
+      githubUrl: "https://github.com/faidrn/instagram-clone",
+      demoUrl: "https://faidrn.github.io/instagram-clone/"
+    },
+    {
+      id: 6,
+      title: "Neon Text Effect",
+      description: "Neon text effect with glowing edges, mimicking the look of neon lights",
+      technologies: ["HTML", "CSS", "AI-powered"],
+      githubUrl: "https://github.com/faidrn/texto-neon-con-html-css",
+      demoUrl: "https://faidrn.github.io/texto-neon-con-html-css/"
+    },
+    {
+      id: 7,
+      title: "Digital Clock",
+      description: "Modern digital clock that displays the current time in hours, minutes, and seconds, with a real-time update every second.",
+      technologies: ["HTML", "CSS", "JavaScript", "AI-powered"],
+      githubUrl: "https://github.com/faidrn/digital-clock", 
+      demoUrl: "https://faidrn.github.io/digital-clock/"
+    },
+    {
+      id: 8, 
+      title: "Analog Clock",
+      description: "Functional analog clock with a round clock face and moving hour, minute, and second hands. The clock updates every second, mimicking the movement of a traditional analog clock.",
+      technologies: ["HTML", "CSS", "JavaScript"], 
+      githubUrl: "https://github.com/faidrn/analog-clock", 
+      demoUrl: "https://faidrn.github.io/analog-clock/"
+    },
+    {
+      id: 9,
+      title: "Hangman Game", 
+      description: "It's a guessing game about the player tries to guess a word by suggesting letters within a certain number of guesses. Originally a Paper-and-pencil game, this is HTML, CSS and JavaScript's version.",
+      technologies: ["HTML", "CSS", "JavaScript"], 
+      githubUrl: "https://github.com/faidrn/hangman-game", 
+      demoUrl: "https://faidrn.github.io/hangman-game/"
+    
+    }, 
+    {
+      id: 10, 
+      title: "Youtube Clone",
+      description: "YouTube Clone as a project to gain practical experience in web development and understand the technologies used in this field. This project involves developing a web application that looks like YouTube.",
+      technologies: ["HTML", "CSS", "JavaScript"], 
+      githubUrl: "https://github.com/faidrn/youtube-clone", 
+      demoUrl: "https://faidrn.github.io/youtube-clone/"
+    }, 
+    {
+      id: 11, 
+      title: "Book in 3D with CSS",
+      description: "Drawing of a book in 3D with HTML and CSS.",
+      technologies: ["HTML", "CSS"], 
+      githubUrl: "https://github.com/faidrn/hello-world/blob/main/book-3d.html", 
+      demoUrl: "https://faidrn.github.io/hello-world/book-3d.html"
+    }, 
+    {
+      id: 12, 
+      title: "House in 3D with CSS, Dragon Ball Z", 
+      description: "Drawing from the kame house in 3D from the anime Dragon Ball Z; using HTML, CSS and JavaScript.",
+      technologies: ["HTML", "CSS", "JavaScript"], 
+      githubUrl: "https://github.com/faidrn/hello-world/tree/main/kame-house", 
+      demoUrl: "https://faidrn.github.io/hello-world/kame-house/kame-house.html"
+    }, 
+    {
+      id: 13, 
+      title: "Savings Capacity Simulator",
+      description: "Simulator that calculates the possibility of separating a percentage of the income and reserving it for future use.",
+      technologies: ["HTML", "CSS", "JavaScript"], 
+      githubUrl: "https://github.com/faidrn/hello-world/tree/main/JavaScript/salary-study-simulator", 
+      demoUrl: "https://faidrn.github.io/hello-world/JavaScript/salary-study-simulator/capacidad-de-ahorro.html"
+    }, 
+    {
+      id: 14, 
+      title: "Single Section Landing Pages",
+      description: "PlatziWebChallange - Projects to grow as a Web Developer", 
+      technologies: ["HTML", "CSS"], 
+      githubUrl: "https://github.com/faidrn/landing-page-con-unica-seccion-de-introduccin", 
+      demoUrl: "https://faidrn.github.io/landing-page-con-unica-seccion-de-introduccin/"
+    }, 
+    {
+      id: 15, 
+      title: "Challenge Article Preview Component", 
+      description: "PlatziWebChallange - This challenge applies basic knowledge of HTML, CSS and JavaScript",
+      technologies: ["HTML", "CSS", "JavaScript"],
+      githubUrl: "https://github.com/faidrn/challenge-article-preview-component", 
+      demoUrl: "https://faidrn.github.io/challenge-article-preview-component/"
+    }, 
+    {
+      id: 16, 
+      title: "Challenge Four Card Feature Section", 
+      description: "PlatziWebChallange - This challenge applies basic knowledge of HTML, CSS", 
+      technologies: ["HTML", "CSS"], 
+      githubUrl: "https://github.com/faidrn/challenge-four-card-feature-section", 
+      demoUrl: "https://faidrn.github.io/challenge-four-card-feature-section/"
+    }, 
+    {
+      id: 17,
+      title: "Hangman Game",
+      description: "It's a guessing game about the player tries to guess a word by suggesting letters within a certain number of guesses. Originally a Paper-and-pencil game, this version was made with Python.", 
+      technologies: ["Python"], 
+      githubUrl: "https://github.com/faidrn/hangman-game-py", 
+      demoUrl: ""
     }
   ];
