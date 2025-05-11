4ef293b256d6bec3ab328798722eedf493fbedc3
Fredy Izquierdo <fredy_izquierdo@hotmail.es>
2025-05-11 20:54:31 +0200

    feat: Add styles to the contact section

--- src/components/sections/Contact/Contact.js
+++ src/components/sections/Contact/Contact.js
@@ -6,15 +6,32 @@ import './Contact.css';
 
 function Contact(){
     return (
-        <section className="contact-container">
-            <div className="contact-image">
+        <section className="contact-section">
+            {/* <div className="contact-image">
                 <FiCode size={120} className="code-icon" /> 
                 <FiMail size={80} className="mail-icon" />
+            </div> */}
+            <div className="about-container">
+                <h2 className="contact-title">Get in touch!</h2>
+                
+                <div className="about-grid">
+                    {/* Columna izquierda - Foto */}
+                    <div className="image-container">
+                        <img className="contact-image" src="/assets\images\contact-img.png" alt="Contact" />
+                    </div>
+
+                    {/* Columna derecha - Información */}
+                    <div className="contact-content">
+                        <p className="contact-info">Always available for freelancing if the right project
+                            comes along, Feel free to contact me.</p>
+                        
+                        <div className="contact-form">
+                            <ContactForm /> {/* Componente reutilizable */}
+                        </div>
+
+                    </div>
+                </div>
             </div>
-            <h2 className="contact-title">Get in touch!</h2>
-            <p>Always available for freelancing if the right project
-            comes along, Feel free to contact me.</p>
-            <ContactForm /> {/* Tu componente reutilizable */}
         </section>
     );
 }
